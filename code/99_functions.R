95c8fafb94bc9552ad8d07f88fd643183bd45fbe
Tess Farr <mtf23081@gmail.com>
2025-01-26 18:35:10 +0000

    Added Hofstede index variables. Reorganized code.

--- code/99_functions.R
+++ code/99_functions.R
@@ -9,10 +9,13 @@ read_workbook <- function(file){
 }
 
 # change country names where they don't match between tables
-change_names <- function(name, n_tbl){
-  if(name %in% n_tbl$name_old) {
-    ret_val <- n_tbl |> filter(name_old == name) |> pull(name_new)
+change_names <- function(name, name_new, n_tbl){
+  if(name %in% (n_tbl |> pull({{ name_new }}))){
+    ret_val <- n_tbl |> filter( {{ name_new }} == name) |> pull(name_old)
   } else {
     ret_val <- name
   }
 }
+
+
+# add appropriate missing country names back to table
